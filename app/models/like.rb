24f7e99e319406f615accd54373d945b6ce5ea30
smhammadzaidi <smhammadzaidi@gmail.com>
2025-03-14 17:27:02 +0000

    erd updated

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  fan_id     :bigint
+#  photo_id   :bigint
+#
+# Indexes
+#
+#  index_likes_on_fan_id               (fan_id)
+#  index_likes_on_fan_id_and_photo_id  (fan_id,photo_id) UNIQUE
+#  index_likes_on_photo_id             (photo_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (fan_id => users.id)
+#  fk_rails_...  (photo_id => photos.id)
+#
 class Like < ApplicationRecord
   belongs_to :fan, class_name: "User", foreign_key: :fan_id
   belongs_to :photo, counter_cache: true

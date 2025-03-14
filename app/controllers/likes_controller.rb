23846e8b7071db8b752363815d1ced1da3e77fb2
smhammadzaidi <smhammadzaidi@gmail.com>
2025-03-15 00:13:18 +0500

    moreeee

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,7 +4,7 @@ class LikesController < ApplicationController
     @like.fan = current_user
     
     if @like.save
-      redirect_to root_path, notice: "Like created successfully."
+      redirect_to @like.photo, notice: "Like created successfully."
     else
       redirect_to @like.photo, alert: "Photo could not be liked."
     end
@@ -16,7 +16,7 @@ class LikesController < ApplicationController
     if @like.fan == current_user
       photo = @like.photo
       @like.destroy
-      redirect_to root_path, alert: "Like deleted successfully."
+      redirect_to photo, notice: "Like deleted successfully."
     else
       redirect_to @like.photo, alert: "You are not authorized to perform this action."
     end
